4612e271384be4450fc1bac7dbb9c421c8e6d3e7
Kaique Mascarenhas <oikemascarenhas@gmail.com>
2023-12-18 10:55:18 -0300

    Update-comment

--- database.php
+++ database.php
@@ -10,7 +10,7 @@
                             $db_user, 
                             $db_password, 
                             $db_name);
-    }//Se a conexão não for bem-sucedida vai pro bloco catch que vai tem o erro como parâmetro
+    }//Se a conexão não for bem-sucedida vai pro bloco catch que tem o erro como parâmetro
     catch(mysqli_sql_exception){
         echo "Could not connect <br>";
     }
